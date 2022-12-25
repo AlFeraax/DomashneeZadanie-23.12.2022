afd1922390980d2d5ac9bf1f30adfca76d5d9244
AlFeraax <grigolin@mail.ru>
2022-12-25 16:23:00 +0300

    доп задача сделанa

---
+++ DopZadacha/Program.cs
@@ -0,0 +1,8 @@
+﻿// Дополнительная задача.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int x = number;
+string chislo = Convert.ToString(x, 2);
+
+Console.WriteLine($"В двоичной системе число {number} записывается как {chislo}.");
